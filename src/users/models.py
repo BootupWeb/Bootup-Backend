938a82772a90a7ac4a76aeb48b52cc9b64e1863a
omar jandali <omarjandali@omars-mbp.lan>
2018-12-19 14:44:00 -0800

    created and migrated the users credentials model

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 class Credentials(models.Model):
-  username = models.CharField(max_length=22, null=false)
-  password = models.CharField(max_length=200, null=false)
-  email = models.CharField(max_length=66, null=false)
-  
+    username = models.CharField(max_length=22)
+    password = models.EmailField(max_length=200)
+    email = models.CharField(max_length=66)
+
+    def __str__(self):
+        return self.username
