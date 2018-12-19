ec41849a1465f37e35e9a5b75ee56e0af799d64d
omar jandali <omarjandali@omars-mbp.lan>
2018-12-19 14:30:29 -0800

    added rest frameowkr settings and created the inital credentials model for users

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Credentials(models.Model):
+  username = models.CharField(max_length=22, null=false)
+  password = models.CharField(max_length=200, null=false)
+  email = models.CharField(max_length=66, null=false)
+  
