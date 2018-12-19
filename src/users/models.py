4e3a9472b2f98107505a6f3bca2a91576dbd60d6
omar jandali <omarjandali@omars-mbp.lan>
2018-12-19 14:57:02 -0800

    altered the credentials model, created sample credentials, linked admin site for backend

--- src/users/models.py
+++ src/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Credentials(models.Model):
     username = models.CharField(max_length=22)
-    password = models.EmailField(max_length=200)
-    email = models.CharField(max_length=66)
+    password = models.CharField(max_length=22)
+    email = models.EmailField(max_length=200)
 
     def __str__(self):
         return self.username
