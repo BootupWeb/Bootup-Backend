c371a9a85bf461964b58f2e81ad9bf7c55ed4f00
omar jandali <omarjandali@omars-mbp.lan>
2018-12-26 20:45:44 -0800

    made a few changes to the serializers and model

--- src/campaign/api/serializers.py
+++ src/campaign/api/serializers.py
@@ -5,4 +5,4 @@ from campaign.models import Detail
 class DetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Detail
-        fields = ('name', 'category', 'sub_category', 'established', 'url', 'demo', 'video', 'location_city', 'location_state', 'progress', 'status', 'phone')
+        fields = ('name', 'category', 'established', 'url', 'demo', 'video', 'location_city', 'location_state', 'progress', 'status', 'phone')
---
+++ src/campaign/choices.py
@@ -0,0 +1,103 @@
+AGRICULTURE = 'Agriculture'
+ASSISTIVE = 'Assistive'
+BALANCING = 'Balancing'
+BANKING = 'Banking'
+BIOTECH = 'Biotech'
+BUILDING = 'Building'
+CLEANING = 'Cleaning'
+CYBERNETICS = 'Cybernetics'
+DOMESTIC = 'Domestic'
+DRILLING = 'Drilling'
+EDUCATIONAL = 'Educational'
+ELECTRONICS = 'Electronics'
+EMERGING = 'Emerging'
+ENERGY = 'Energy'
+ENVIRONMENTAL = 'Environmental'
+FILMANDVIDEO = 'Film and Video'
+FINANCIAL = 'Financial'
+FOOD = 'Food'
+HOME = 'Home'
+INFORMATION = 'Information'
+MEDICAL = 'Medical'
+MICRO = 'Microtechnology'
+MILITARY = 'Military'
+MINING = 'Mining'
+AUTOMOTIVE = 'Automotive'
+MUSIC = 'Music'
+NANOTECH = 'Nanotechnology'
+SPACE = 'Space'
+SPORTS = 'Sports'
+TRANSPORTATION = 'Transportation'
+TRAVEL = 'Travel'
+
+CATEGORY_CHOICES = (
+    (AGRICULTURE, 'Agriculture'),
+    (ASSISTIVE, 'Assistive'),
+    (BALANCING, 'Balancing'),
+    (BANKING, 'Banking'),
+    (BIOTECH, 'Biotech'),
+    (BUILDING, 'Building'),
+    (CLEANING, 'Cleaning'),
+    (CYBERNETICS, 'Cybernetics'),
+    (DOMESTIC, 'Domestic'),
+    (DRILLING, 'Drilling'),
+    (EDUCATIONAL, 'Educational'),
+    (ELECTRONICS, 'Electronics'),
+    (EMERGING, 'Emerging'),
+    (ENERGY, 'Energy'),
+    (ENVIRONMENTAL, 'Environmental'),
+    (FILMANDVIDEO, 'Film and Video'),
+    (FINANCIAL, 'Financial'),
+    (FOOD, 'Food'),
+    (HOME, 'Home'),
+    (INFORMATION, 'Information'),
+    (MEDICAL, 'Medical'),
+    (MICRO, 'Microtechnology'),
+    (MILITARY, 'Military'),
+    (MINING, 'Mining'),
+    (AUTOMOTIVE, 'Automotive'),
+    (MUSIC, 'Music'),
+    (NANOTECH, 'Nanotechnology'),
+    (SPACE, 'Space'),
+    (SPORTS, 'Sports'),
+    (TRANSPORTATION, 'Transportation'),
+    (TRAVEL, 'Travel')
+)
+
+WEB = 'Web'
+IOS = 'iOS'
+ANDROID = 'Android'
+OSX = 'osX'
+WINDOWS = 'Windos'
+
+PLATFORM_CHOICES = (
+    (WEB, 'Web'),
+    (IOS, 'iOS'),
+    (ANDROID, 'Android'),
+    (OSX, 'oSX'),
+    (WINDOWS, 'Windows')
+)
+
+ALPHA = 'Alpha'
+CLOSED_BETA = 'Closed Beta'
+OPEN_BETA = 'Open Beta'
+PRODUCTION = 'Production'
+
+PROGRESS_CHOICES = (
+    (ALPHA, 'Alpha'),
+    (CLOSED_BETA, 'Closed Beta'),
+    (OPEN_BETA, 'Open Beta'),
+    (PRODUCTION, 'Production')
+)
+
+STARTUP = 'Startup'
+SMALL = 'Small'
+MID_SIZE = 'Mid Size'
+LARGE = 'Large'
+
+SIZE_CHOICES = (
+    (STARTUP, 'Startup'),
+    (SMALL, 'Small'),
+    (MID_SIZE, 'Mid Size'),
+    (LARGE, 'Large')
+)
