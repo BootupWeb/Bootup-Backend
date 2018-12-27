4378dbfebf5b095a7c961576ef8e4d2f784982a7
omar jandali <omarjandali@omars-mbp.lan>
2018-12-26 21:07:23 -0800

    added the story to the admin site

--- src/campaign/admin.py
+++ src/campaign/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Detail
+from .models import Detail, Story
 
 admin.site.register(Detail)
+admin.site.register(Story)
