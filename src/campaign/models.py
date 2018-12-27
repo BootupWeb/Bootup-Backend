be49d3f819b8aa46cea04e5697eb2c47b195cc50
omar jandali <omarjandali@omars-mbp.lan>
2018-12-26 16:56:11 -0800

    created the detail model to the application. tested to see how to create fields with different choice options. coorect integration

--- src/campaign/models.py
+++ src/campaign/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+FINANCIAL = 'Financial'
+GREEN_ENERGY = 'Green Energy'
+
+CATEGORY_CHOICES = (
+    (FINANCIAL, 'financial'),
+    (GREEN_ENERGY, 'green energy'),
+)
+
+class Detail(models.Model):
+    name = models.CharField(max_length=32)
+    category = models.CharField(max_length=32, choices=CATEGORY_CHOICES, default=FINANCIAL)
+    sub_category = models.CharField(max_length=32) # add choices
+    established = models.SmallIntegerField()
+    url = models.URLField(max_length=200)
+    demo = models.URLField(max_length=200)
+    video = models.URLField(max_length=200)
+    location_city = models.CharField(max_length=32)
+    location_state = models.CharField(max_length=22)
+    phone = models.CharField(max_length=13)
+    progress = models.CharField(max_length=12)
+    status = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
