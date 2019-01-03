dada645e296e2ce700e2e53e7b1836bd883cc7f7
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-03 14:21:24 -0800

    added a cors header middleware so that I can make requirest to the api

--- src/bootup/settings.py
+++ src/bootup/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'users',
     'campaign',
     'chat',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
