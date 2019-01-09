0bac4e2439a90528196d72d4a7cfa62282177198
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-08 16:48:09 -0800

    updated and made changes to the campaign models

--- src/campaign/api/serializers.py
+++ src/campaign/api/serializers.py
@@ -5,15 +5,16 @@ from campaign.models import Detail, Story, Update
 class DetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Detail
-        fields = ('name', 'category', 'established', 'url', 'demo', 'video', 'location_city', 'location_state', 'progress', 'status', 'phone')
+        fields = ('name', 'slug', 'category', 'established', 'url', 'video', 'location_city', 'location_state', 'progress', 'status', 'equity', 'amount',
+                    'valuation', 'features_image')
 
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
-        fields = ('details', 'slug', 'product', 'summary', 'pick_project', 'far_along', 'competitors', 'different', 'new', 'money_use', 'user_aquisition',
-                  'monetization', 'user_count', 'equity_summary', 'future_six', 'future_one', 'future_two', 'future_three', 'other_projects')
+        fields = ('details', 'product', 'pick_project', 'far_along', 'competitors', 'different', 'new', 'money_use', 'user_aquisition',
+                  'monetization', 'user_count', 'future_six', 'future_one', 'future_two', 'future_three', 'other_projects')
 
 class UpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Update
-        fields = ('campaign', 'message', 'impact')
+        fields = ('campaign', 'message')
--- src/campaign/models.py
+++ src/campaign/models.py
@@ -4,54 +4,50 @@ from .choices import CATEGORY_CHOICES, PLATFORM_CHOICES, PROGRESS_CHOICES, SIZE_
 
 class Detail(models.Model):
     name = models.CharField(max_length=32)
+    slug = models.CharField(max_length=225)
     category = models.CharField(max_length=32, choices=CATEGORY_CHOICES)
-    #sub_category = models.CharField(max_length=32) # add choices
     platform = models.CharField(max_length=22, choices=PLATFORM_CHOICES)
     established = models.SmallIntegerField()
     url = models.URLField(max_length=200)
-    demo = models.URLField(max_length=200)
     video = models.URLField(max_length=200)
     location_city = models.CharField(max_length=32)
     location_state = models.CharField(max_length=22)
-    phone = models.CharField(max_length=13)
     progress = models.CharField(max_length=22, choices=PROGRESS_CHOICES)
     status = models.CharField(max_length=22, choices=SIZE_CHOICES)
+    equity = models.DecimalField(max_digits=3, decimal_places=2)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    valuation = models.DecimalField(max_digits=15, decimal_places=2)
+    featured_images = models.ImageField(upload_to='featured_images/')
     created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 class Story(models.Model):
-    details = models.ForeignKey(Detail, on_delete=models.CASCADE)
-    slug = models.CharField(max_length=225)
+    detail = models.ForeignKey(Detail, on_delete=models.CASCADE)
     product = models.TextField()
-    summary = models.TextField()
-    pick_project = models.TextField()
     far_along = models.TextField()
-    competitors = models.TextField()
-    different = models.TextField()
+    users_count = models.TextField()
     new = models.TextField()
-    money_use = models.TextField()
+    different = models.TextField()
+    competitors = models.TextField()
     user_aquisition = models.TextField()
     monetization = models.TextField()
-    users_count = models.TextField()
-    equity_summary = models.TextField()
+    pick_project = models.TextField()
+    money_use = models.TextField()
     future_six = models.TextField()
     future_one = models.TextField()
     future_two = models.TextField()
     future_three = models.TextField()
     other_projects = models.TextField()
     created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.details.name + ' - ' + self.slug
+        return self.details.name + ' - ' + self.product
 
 class Update(models.Model):
     campaign = models.ForeignKey(Detail, on_delete=models.CASCADE)
     message = models.TextField()
-    impact = models.TextField()
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
 
